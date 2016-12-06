4c35061a41e32a2c704374fa20e2abf4298b5319
Oriol Boix Anfosso <dev@orboan.com>
2016-12-06 15:45:28 -0500

    create_user.sh overriden

---
+++ container-files/config/init/create_user.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+user=www
+# Creating the www user only if it does not exist
+ret=false
+getent passwd $user >/dev/null 2>&1 && ret=true
+
+if $ret; then
+echo "user already exists";
+else
+
+useradd $user -d /data/$user
+# Setting password for the www user
+echo "${user}:iaw" | chpasswd
+# Add 'www' user to sudoers
+echo "${user}  ALL=(ALL)  NOPASSWD: ALL" > /etc/sudoers.d/$user
+echo "user created"
+fi
